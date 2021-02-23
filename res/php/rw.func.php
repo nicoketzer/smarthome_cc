39e538ef4a688db3c6bb705a519d5e8c0c9b581e
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2021-02-23 16:39:33 +0100

    Create rw.func.php
    Hinzufügen der Funktionen:
    read_file();
    write_file();

---
+++ res/php/rw.func.php
@@ -0,0 +1,32 @@
+<?php 
+if(!function_exists("read_file")){
+function read_file($file){
+    if(file_exists($file)){
+        $fs = filesize($file);
+        if($fs >= 1){
+            //Dateiinhalt auslesen
+            $handle = fopen($file,"r");
+            $back = fread($handle,$fs);
+            fclose($handle);
+            //Ausgelesene Datei zurückgeben
+            return $back;
+        }else{
+            //Dateigröße = 0 sprich leer
+            //Leeren String zurückgeben
+            return "";
+        }
+    }else{
+        //Datei existiert nicht also leeren String zurückgeben
+        return "";
+    }
+}
+}
+if(!function_exists("write_file")){
+function write_file($dateiname,$dateiinhalt,$modus){
+    $handle = fopen($dateiname,$modus);
+    fwrite($handle,$dateiinhalt);
+    fclose($handle);
+    return true;
+}
+}
+?>
