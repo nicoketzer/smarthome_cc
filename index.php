8a93740ba8d69302e828f09960639cc2bbe49a38
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2021-02-23 10:31:46 +0100

    Update index.php
    Hinzufügen von Inhalt in die index.php
    
    -->Kein PHP-Code vorhanden
    -->Es beleibt trdm. eine .php Datei da eine index.php meist bevorzugt zu einer index.html aufgerufen wird

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+?>
+<html>
+    <head>
+        <title>C&amp;C - Server Page</title>
+    </head>
+    <body>
+        <h1>C&amp;C-Server Start-Page</h1>
+        <p>Solltest du diese Seite sehen war die installation des C&amp;C-Server warscheindlich 
+        erfolgreich.</p>
+        <br />
+        <p>Solltest du Admin sein so kannst du &uuml;ber das Terminal den Bind-Token auslesen und 
+        diesen C&amp;C-Server nun mit einem Webinterface verbinden.</p>
+        <br />
+        <p>Die Steuerung erfolgt ausschlie&szlig;lich &uuml;ber das Webinterface.</p>
+        <br />
+        <br />
+        <p>Mehr zum Projekt "Smarthome" findest du auf Github unter <a href="https://github.com/nicoketzer/smarthome">https://github.com/nicoketzer/smarthome</a></p>
+    </body>
+</html>
