d0a32d1fd2d4240b65dbe0d6d61c628cb9194e90
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2021-04-15 20:21:26 +0200

    Create var_tmp.php
    Hinzufügen einer Temporären VAR Datei wo Temporär der Admin-Token für das Webinterface hinterlegt wird damit alle Anderen Daten geholt werden können

---
+++ res/php/var_tmp.php
@@ -0,0 +1,10 @@
+<?php
+    #Verbindungstoken
+    $conn_token = "__CONN_TOKEN__";
+    #Server - Addresse (egal ob IP oder DDNS bzw. Domain)
+    $server_addr = "__SERVER_ADDR__";
+    #Kommunikations-Port
+    $server_port = "__SERVER_PORT__";
+    #Protukoll (http oder https)
+    $server_prot = "__SERVER_PROT__";
+?>
